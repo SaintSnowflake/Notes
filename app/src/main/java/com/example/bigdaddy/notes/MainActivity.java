6f37faf6ef09c4d62b20f4d804ea4db8a3d51038
Mikhaylova Kseniya <snowflake2552@mail.ru>
2016-09-23 08:57:10 +0300

    Test

--- app/src/main/java/com/example/bigdaddy/notes/MainActivity.java
+++ app/src/main/java/com/example/bigdaddy/notes/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.bigdaddy.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        Button add_note_button = (Button) findViewById(R.id.add_note_button);
+        add_note_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_note_button:
+                //startActivity(MainActivity.this, AddNoteActivity.class);
+                break;
+        }
     }
 }
